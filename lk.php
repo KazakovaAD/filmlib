77d67a4973efae15015ea09eed065ff8e9495dd4
KazakovaAD <kazakova.nd@mail.ru>
2020-09-28 14:23:07 +0400

    доработала форму авторизации

--- lk.php
+++ lk.php
@@ -37,23 +37,22 @@
 <body>
 <form class="form-signin">
     <div class="text-center mb-4">
-        <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
         <h1 class="h3 mb-3 font-weight-normal">Авторизация</h1>
     </div>
 
     <div class="form-label-group">
         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputEmail">Email address</label>
+        <label for="inputEmail">Почта</label>
     </div>
 
     <div class="form-label-group">
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <label for="inputPassword">Password</label>
+        <label for="inputPassword">Пароль</label>
     </div>
 
     <div class="checkbox mb-3">
         <label>
-            <input type="checkbox" value="remember-me"> Remember me
+            <input type="checkbox" value="remember-me"> Запомнить пароль
         </label>
     </div>
     <button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button>
