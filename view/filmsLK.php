7aa10e7d0d430782dba9e532319f4164a677df97
KazakovaAD <kazakova.nd@mail.ru>
2020-11-19 02:19:38 +0400

    lab4

---
+++ view/filmsLK.php
@@ -0,0 +1,56 @@
+<main role="main">
+
+    <div class="album py-5 bg-light">
+        <div class="container margin">
+            <h1>Мои фильмы</h1>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mb-4 shadow-sm text">
+                        <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/site/image/lk_1.jpg"><text x="50%" y="50%" fill="#eceeef" dy=".3em"><b>Криминальное чтиво</b></text></img>
+                        <div class="card-body">
+                            <p class="card-text">Двое бандитов Винсент Вега и Джулс Винфилд ведут философские беседы в перерывах между разборками и решением проблем с должниками криминального босса Марселласа Уоллеса.</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Показать</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Удалить</button>
+                                </div>
+                                <small class="text-muted">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4 shadow-sm text">
+                        <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/site/image/lk_2.jpg"><text x="50%" y="50%" fill="#eceeef" dy=".3em"><b>Гладиатор</b></text></img>
+                        <div class="card-body">
+                            <p class="card-text">В великой Римской империи не было военачальника, равного генералу Максимусу. Но готовый сразиться с любым противником в честном бою, он оказался бессилен против вероломных придворных интриг. Генерала предали и приговорили к смерти. Чудом избежав гибели, Максимус становится гладиатором.</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Показать</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Удалить</button>
+                                </div>
+                                <small class="text-muted">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4 shadow-sm text">
+                        <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/site/image/lk_3.jpg"><text x="50%" y="50%" fill="#eceeef" dy=".3em"><b>Джанго освобожденный</b></text></img>
+                        <div class="card-body">
+                            <p class="card-text">Эксцентричный охотник за головами, также известный как Дантист, промышляет отстрелом самых опасных преступников. Работенка пыльная, и без надежного помощника ему не обойтись. Освобождённый им раб по имени Джанго – прекрасная кандидатура.</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Показать</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Удалить</button>
+                                </div>
+                                <small class="text-muted">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
